8dbc0a9ceef7e0119f71c52c0ab66fbf7bd6f425
shaojianbin <shaojianbin@yixin.im>
2023-12-28 14:14:47 +0800

    合规检测，替换文字

--- docs/zh-cn/resources/resources.md
+++ docs/zh-cn/resources/resources.md
@@ -16,7 +16,7 @@
 1. 使用游戏`线上keystore`对游戏进行签名
 2. 在安卓机上安装已签名的游戏
 3. 在安卓机上安装GetSignature.apk
-4. 运行GetSignature.apk，填入游戏包名，生成签名，配置到易信游戏后台对应的游戏下面
+4. 运行GetSignature.apk，填入游戏包名，生成签名，配置到后台对应的游戏下面
 
 #### 自测文档
 [下载地址](http://nosdn-yx.127.net/yxgame/8a49e8136628c1520166291f78bb0001.xlsx)
@@ -24,5 +24,5 @@
 ## 注意事项
 1. 先申请GameID，GameSecret，交给对应开发
 2. 为保证授权，支付等可以正常调试，调试前请确认后台相关包名，签名是否配置正确
-3. 包名后缀cloudgame (例com.netease.yys.cloudgame，如果易信后台已经配置了包名后缀为yixin，则与后台配置保持一致)
+3. 包名后缀cloudgame (例com.netease.yys.cloudgame，如果后台已经配置了包名后缀为yixin，则与后台配置保持一致)
 4. APK不需要角标，需要闪屏
--- site_config/home.jsx
+++ site_config/home.jsx
@@ -1,93 +1,93 @@
 import React from 'react';
 
 export default {
-  'zh-cn': {
-    brand: {
-      brandName: '易信游戏',
-      briefIntroduction: '游戏客户端，服务器接入文档',
-      buttons: [
-        {
-          text: '游戏客户端',
-          link: '/zh-cn/docs/client/gamesdk.html',
-          type: 'primary',
+    'zh-cn': {
+        brand: {
+            brandName: '游戏SDK',
+            briefIntroduction: '游戏客户端，服务器接入文档',
+            buttons: [
+                {
+                    text: '游戏客户端',
+                    link: '/zh-cn/docs/client/gamesdk.html',
+                    type: 'primary',
+                },
+                {
+                    text: '服务器',
+                    link: '/zh-cn/docs/server/cloudgame-server-doc.html',
+                    type: 'normal',
+                },
+            ],
         },
-        {
-          text: '服务器',
-          link: '/zh-cn/docs/server/cloudgame-server-doc.html',
-          type: 'normal',
+        introduction: {
+            title: '开源产品的介绍',
+            desc: '开源产品的简单介绍，提供一些该产品的优点、特性描述等',
+            img: '/img/architecture.png',
         },
-      ],
+        features: {
+            title: '特性一览',
+            list: [
+                {
+                    img: '/img/feature_transpart.png',
+                    title: '特性1',
+                    content: '特性1的简单概括',
+                },
+                {
+                    img: '/img/feature_loadbalances.png',
+                    title: '特性2',
+                    content: '特性2的简单概括',
+                },
+                {
+                    img: '/img/feature_service.png',
+                    title: '特性3',
+                    content: '特性3的简单概括',
+                },
+                {
+                    img: '/img/feature_hogh.png',
+                    title: '特性4',
+                    content: '特性4的简单概括',
+                },
+                {
+                    img: '/img/feature_runtime.png',
+                    title: '特性5',
+                    content: '特性5的简单概括',
+                },
+                {
+                    img: '/img/feature_maintenance.png',
+                    title: '特性6',
+                    content: '特性6的简单概括',
+                },
+            ],
+        },
+        start: {
+            title: '快速开始',
+            desc: '简单描述',
+            img: '/img/quick_start.png',
+            button: {
+                text: '阅读更多',
+                link: '/zh-cn/docs/demo1.html',
+            },
+        },
+        users: {
+            title: '用户',
+            desc: <span>简单描述</span>,
+            list: [
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+                '/img/users_alibaba.png',
+            ],
+        },
     },
-    introduction: {
-      title: '开源产品的介绍',
-      desc: '开源产品的简单介绍，提供一些该产品的优点、特性描述等',
-      img: '/img/architecture.png',
-    },
-    features: {
-      title: '特性一览',
-      list: [
-        {
-          img: '/img/feature_transpart.png',
-          title: '特性1',
-          content: '特性1的简单概括',
-        },
-        {
-          img: '/img/feature_loadbalances.png',
-          title: '特性2',
-          content: '特性2的简单概括',
-        },
-        {
-          img: '/img/feature_service.png',
-          title: '特性3',
-          content: '特性3的简单概括',
-        },
-        {
-          img: '/img/feature_hogh.png',
-          title: '特性4',
-          content: '特性4的简单概括',
-        },
-        {
-          img: '/img/feature_runtime.png',
-          title: '特性5',
-          content: '特性5的简单概括',
-        },
-        {
-          img: '/img/feature_maintenance.png',
-          title: '特性6',
-          content: '特性6的简单概括',
-        },
-      ],
-    },
-    start: {
-      title: '快速开始',
-      desc: '简单描述',
-      img: '/img/quick_start.png',
-      button: {
-        text: '阅读更多',
-        link: '/zh-cn/docs/demo1.html',
-      },
-    },
-    users: {
-      title: '用户',
-      desc: <span>简单描述</span>,
-      list: [
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-      ],
-    },
-  },
 };
